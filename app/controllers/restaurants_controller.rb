36f1ec6bda468fd52855ffdca3d00f4b2da21838
Tim Oxman <tim.oxman@gmail.com>
2015-06-15 21:01:54 +0100

    delete working

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -22,14 +22,23 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
+# intention to alter record
   def edit
     @restaurant = Restaurant.find(params[:id])
   end
 
+# record is actually updated
   def update
     @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
 
+    redirect_to '/restaurants'
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = 'Restaurant deleted successfully'
     redirect_to '/restaurants'
   end
 
