09f16c07cbcd816dde1f28de223ec1d5930ccb39
Tim Oxman <tim.oxman@gmail.com>
2015-06-15 17:50:36 +0100

    test three passing, add a restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,5 +5,17 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+
   end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+# define which params we are going to allow us to pass to controller
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
 end
