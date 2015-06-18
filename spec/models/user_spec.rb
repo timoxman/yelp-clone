c70d1d60d32e031f851ed950b16ba5b38408fc24
Tim Oxman <tim.oxman@gmail.com>
2015-06-18 09:51:44 +0100

    end of wednesday

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,6 @@ require 'spec_helper'
 describe User, type: :model do
   it { is_expected.to have_many :restaurants }
   it { is_expected.to have_many :reviews }
+#the above two lines are equivelent of the line below..
+  it { is_expected.to have_many :reviewed_restaurants}
 end
